fbfa1e5491cfc0ac4d6306e727379d6adc0318ff
Halil Akgun <halilakgun0@gmail.com>
2025-02-22 00:57:47 +0100

    LC006_ZigzagConversion

--- src/TopInterview150/LC006_ZigzagConversion.java
+++ src/TopInterview150/LC006_ZigzagConversion.java
@@ -1,5 +1,9 @@
 package TopInterview150;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 /*
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
 (you may want to display this pattern in a fixed font for better legibility)
@@ -36,10 +40,63 @@ public class LC006_ZigzagConversion {
         System.out.println(convert("PAYPALISHIRING", 3)); // PAHNAPLSIIGYIR
         System.out.println(convert("PAYPALISHIRING", 4)); // PINALSIGYAHRPI
         System.out.println(convert("A", 1)); // A
+        System.out.println(convert("ABC", 2)); // ACB
 
     }
 
     public static String convert(String s, int numRows) {
-        String[][] a = new String[numRows][];
+        // way 1
+//        if (numRows == 1 || numRows >= s.length()) return s;
+//        String[][] a = new String[numRows][(s.length() / 2) + 1];
+//        c(a, s, numRows);
+//        StringBuilder r = new StringBuilder();
+//        for (String[] strings : a) {
+//            r.append(Arrays.stream(strings).filter(Objects::nonNull).collect(Collectors.joining()));
+//        }
+//        return r.toString();
+
+        // way 2
+        if (numRows == 1 || numRows >= s.length()) return s;
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int i = 0, step = 1;
+        for (char c : s.toCharArray()) {
+            rows[i].append(c);
+            if (i == 0) step = 1;
+            else if (i == rows.length - 1) step = -1;
+            i += step;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
+
+//    private static void c(String[][] a, String s, int numRows) {
+//        int col = 0;
+//        int row = 0;
+//        int idx = 0;
+//        while (true) {
+//            for (int i = 0; i < numRows; i++) {
+//                if (idx == s.length()) return;
+//                a[row++][col] = s.substring(idx, idx + 1);
+//                idx++;
+//            }
+//            row--;
+//            for (int i = 0; i < numRows - 2; i++) {
+//                if (idx == s.length()) return;
+//                a[--row][++col] = s.substring(idx, idx + 1);
+//                idx++;
+//            }
+//            col++;
+//            row--;
+//        }
+//    }
 }
