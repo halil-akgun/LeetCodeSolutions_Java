dc83e44764eeebbbf42c9697c062b3536b7dbb89
Halil Akgun <halilakgun0@gmail.com>
2025-03-01 22:09:35 +0100

    LC015_3Sum

--- src/TopInterview150/LC015_3Sum.java
+++ src/TopInterview150/LC015_3Sum.java
@@ -43,6 +43,7 @@ public class LC015_3Sum {
     public static List<List<Integer>> threeSum(int[] nums) {
         result.clear();
         a = new StringBuilder();
+
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
@@ -53,6 +54,7 @@ public class LC015_3Sum {
                 }
             }
         }
+
         return result;
     }
 
