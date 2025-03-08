cf3456ef70030b65ca5105417d31b36fffce6a91
Halil Akgun <halilakgun0@gmail.com>
2025-03-08 23:38:50 +0100

    LC076_MinimumWindowSubstring

--- src/TopInterview150/LC076_MinimumWindowSubstring.java
+++ src/TopInterview150/LC076_MinimumWindowSubstring.java
@@ -34,40 +34,85 @@ public class LC076_MinimumWindowSubstring {
     }
 
     public static String minWindow(String s, String t) {
+
+        // way 1
+//        Map<Character, Integer> letters = new HashMap<>();
+//        Map<Character, Integer> lettersDuplicate;
+//        int left, right, min = Integer.MAX_VALUE;
+//        int left2 = 0, right2 = -1;
+//
+//        for (int i = 0; i < t.length(); i++) {
+//            letters.put(t.charAt(i), letters.getOrDefault(t.charAt(i), 0) + 1);
+//        }
+//
+//        lettersDuplicate = new HashMap<>(letters);
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char ch = s.charAt(i);
+//            if (letters.containsKey(ch)) {
+//                left = i;
+//                right = i;
+//                letters.put(ch, letters.get(ch) - 1);
+//                while (right < s.length() - 1 && control(letters)) {
+//                    right++;
+//                    char ch2 = s.charAt(right);
+//                    if (letters.containsKey(ch2)) {
+//                        letters.put(ch2, letters.get(ch2) - 1);
+//                    }
+//                }
+//                if (right - left < min && !control(letters)) {
+//                    left2 = left;
+//                    right2 = right;
+//                    min = right - left;
+//                }
+//            }
+//            letters = new HashMap<>(lettersDuplicate);
+//        }
+//        return right2 == -1 ? "" : s.substring(left2, right2 + 1);
+
+        // way 2
+        if (s.length() < t.length()) return "";
+
         Map<Character, Integer> letters = new HashMap<>();
-        Map<Character, Integer> lettersDuplicate;
-        int left, right, min = Integer.MAX_VALUE;
-        int left2 = 0, right2 = s.length() - 1;
-
-        for (int i = 0; i < t.length(); i++) {
-            letters.put(t.charAt(i), letters.getOrDefault(t.charAt(i), 0) + 1);
+        for (char c : t.toCharArray()) {
+            letters.put(c, letters.getOrDefault(c, 0) + 1);
         }
 
-        lettersDuplicate = new HashMap<>(letters);
+        int required = letters.size();
+        int left = 0, right = 0, minLen = Integer.MAX_VALUE;
+        int minLeft = 0, minRight = 0;
+        int formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (letters.containsKey(ch)) {
-                left = i;
-                right = i;
-                letters.put(ch, letters.get(ch) - 1);
-                while (right < s.length()-1 && control(letters)) {
-                    right++;
-                    char ch2 = s.charAt(right);
-                    if (letters.containsKey(ch2)) {
-                        letters.put(ch2, letters.get(ch2) - 1);
-                    }
-                }
-                if (right - left < min) {
-                    left2 = left;
-                    right2 = right;
-                    min = right - left;
-                }
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            windowCounts.put(ch, windowCounts.getOrDefault(ch, 0) + 1);
+
+            if (letters.containsKey(ch) && windowCounts.get(ch).intValue() == letters.get(ch).intValue()) {
+                formed++;
             }
-            letters = lettersDuplicate;
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minLeft = left;
+                    minRight = right;
+                }
+
+                char leftChar = s.charAt(left);
+                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
+
+                if (letters.containsKey(leftChar) && windowCounts.get(leftChar) < letters.get(leftChar)) {
+                    formed--;
+                }
+
+                left++;
+            }
+
+            right++;
         }
-        System.out.println(left2 + " " + right2);
-        return s.substring(left2, right2);
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minRight + 1);
     }
 
     private static boolean control(Map<Character, Integer> letters) {
