bbfbc13093f1f201aac13ba0bd388808ab563c3a
Halil Akgun <halilakgun0@gmail.com>
2025-03-11 00:15:34 +0100

    LC054_SpiralMatrix

--- src/TopInterview150/LC054_SpiralMatrix.java
+++ src/TopInterview150/LC054_SpiralMatrix.java
@@ -16,11 +16,11 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
  */
 public class LC054_SpiralMatrix {
     public static void main(String[] args) {
-//        System.out.println(spiralOrder(new int[][]{
-//                {1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}
-//        })); // [1,2,3,6,9,8,7,4,5]
+        System.out.println(spiralOrder(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        })); // [1,2,3,6,9,8,7,4,5]
         System.out.println(spiralOrder(new int[][]{
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
