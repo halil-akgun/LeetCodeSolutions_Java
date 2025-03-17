571ca8ae551f98cc86dfa9c4096a0490c69e70e5
Halil Akgun <halilakgun0@gmail.com>
2025-03-17 23:13:52 +0100

    LC383_RansomNote

---
+++ src/TopInterview150/LC383_RansomNote.java
@@ -0,0 +1,30 @@
+package TopInterview150;
+
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using
+the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ */
+public class LC383_RansomNote {
+    public static void main(String[] args) {
+        System.out.println(canConstruct("a", "b")); // false
+        System.out.println(canConstruct("aa", "ab")); // false
+        System.out.println(canConstruct("aa", "aab")); // true
+    }
+    public static boolean canConstruct(String ransomNote, String magazine) {
+
+    }
+}
