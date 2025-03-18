63ff8f9452d385aff8469acfedc26d45aeb2072a
Halil Akgun <halilakgun0@gmail.com>
2025-03-18 23:53:31 +0100

    LC383_RansomNote

--- src/TopInterview150/LC383_RansomNote.java
+++ src/TopInterview150/LC383_RansomNote.java
@@ -1,5 +1,7 @@
 package TopInterview150;
 
+import java.util.Arrays;
+
 /*
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using
 the letters from magazine and false otherwise.
@@ -23,8 +25,31 @@ public class LC383_RansomNote {
         System.out.println(canConstruct("a", "b")); // false
         System.out.println(canConstruct("aa", "ab")); // false
         System.out.println(canConstruct("aa", "aab")); // true
+        System.out.println(canConstruct("fihjjjjei", "hjibagacbhadfaefdjaeaebgi")); // false
     }
-    public static boolean canConstruct(String ransomNote, String magazine) {
 
+    public static boolean canConstruct(String ransomNote, String magazine) {
+//        String r = ransomNote;
+//        String m = magazine;
+//
+//        while (!ransomNote.isEmpty()) {
+//            magazine = magazine.replaceAll(ransomNote.charAt(0) + "", "");
+//            ransomNote = ransomNote.replaceAll(ransomNote.charAt(0) + "", "");
+//            if (r.length() - ransomNote.length() > m.length() - magazine.length())
+//                return false;
+//            r = ransomNote;
+//            m = magazine;
+//        }
+//
+//        return true;
+
+        // way 2
+        char[] r = ransomNote.toCharArray();
+        char[] m = magazine.toCharArray();
+        Arrays.sort(r);
+        Arrays.sort(m);
+        for (int i = 0; i < r.length; i++) {
+
+        }
     }
 }
