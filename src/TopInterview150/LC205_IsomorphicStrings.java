f0455366019ef2e5cd6d1cf7c3fce5422e4fad7f
Halil Akgun <halilakgun0@gmail.com>
2025-03-22 23:14:32 +0100

    LC205_IsomorphicStrings

--- src/TopInterview150/LC205_IsomorphicStrings.java
+++ src/TopInterview150/LC205_IsomorphicStrings.java
@@ -1,6 +1,8 @@
 package TopInterview150;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 Given two strings s and t, determine if they are isomorphic.
@@ -34,6 +36,7 @@ public class LC205_IsomorphicStrings {
         System.out.println(isIsomorphic("foo", "bar")); // false
         System.out.println(isIsomorphic("paper", "title")); // true
         System.out.println(isIsomorphic("bbbaaaba", "aaabbbba")); // false
+        System.out.println(isIsomorphic("badc", "baba")); // false
     }
 
     public static boolean isIsomorphic(String s, String t) {
@@ -50,6 +53,24 @@ public class LC205_IsomorphicStrings {
 //        Arrays.sort(lettersT);
 //        return Arrays.equals(lettersS, lettersT);
 
+        // Correct approach
+        if (s.length() != t.length()) return false;
+        char[] lettersOfS = s.toCharArray();
+        char[] lettersOfT = t.toCharArray();
+        Map<Character, Character> map = new HashMap<>();
+        return !(!extracted(lettersOfS, map, lettersOfT) || !extracted(lettersOfT, map, lettersOfS));
+    }
 
+    private static boolean extracted(char[] lettersOfS, Map<Character, Character> map, char[] lettersOfT) {
+        map.clear();
+        for (int i = 0; i < lettersOfS.length; i++) {
+            Character ch = map.get(lettersOfS[i]);
+            if (ch == null) {
+                map.put(lettersOfS[i], lettersOfT[i]);
+            } else if (ch != lettersOfT[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
