b192a588a011157ae71c3e484318fdb86f49407f
Halil Akgun <halilakgun0@gmail.com>
2025-03-25 22:24:44 +0100

    LC242_ValidAnagram

---
+++ src/TopInterview150/LC242_ValidAnagram.java
@@ -0,0 +1,29 @@
+package TopInterview150;
+
+import java.util.Arrays;
+
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+ */
+public class LC242_ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+        System.out.println(isAnagram("rat", "car")); // false
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
+}
