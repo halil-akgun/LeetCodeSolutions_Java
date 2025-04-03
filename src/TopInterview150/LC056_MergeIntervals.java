645a46177f548a0dbfb97d65f57a492a6e8bd6f1
Halil Akgun <halilakgun0@gmail.com>
2025-04-03 22:29:25 +0200

    LC056_MergeIntervals

--- src/TopInterview150/LC056_MergeIntervals.java
+++ src/TopInterview150/LC056_MergeIntervals.java
@@ -1,6 +1,9 @@
 package TopInterview150;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /*
 Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
@@ -23,7 +26,22 @@ public class LC056_MergeIntervals {
     }
 
     public static int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals);
+        if (intervals.length == 0) return new int[0][];
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0])); // sort by start time
 
+        List<int[]> list = new ArrayList<>();
+        int start = intervals[0][0], end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            if (curr[0] > end) {
+                list.add(new int[]{start, end});
+                start = curr[0];
+                end = curr[1];
+            } else {
+                end = Math.max(end, curr[1]);
+            }
+        }
+        list.add(new int[]{start, end});
+        return list.toArray(new int[0][]);
     }
 }
