39ba09259ae2d45dad329cf5ac6001a25c801396
Halil Akgun <halilakgun0@gmail.com>
2025-04-14 21:48:20 +0200

    LC452_MinimumNumberOfArrowsToBurstBalloons

--- src/TopInterview150/LC452_MinimumNumberOfArrowsToBurstBalloons.java
+++ src/TopInterview150/LC452_MinimumNumberOfArrowsToBurstBalloons.java
@@ -43,9 +43,13 @@ public class LC452_MinimumNumberOfArrowsToBurstBalloons {
     public static int findMinArrowShots(int[][] points) {
         if (points.length == 0) return 0;
         Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
-        int arrows = 1;
+        int arrows = 1, end = points[0][1];
         for (int i = 1; i < points.length; i++) {
-            if ()
+            if (points[i][0] > end) {
+                arrows++;
+                end = points[i][1];
+            }
         }
+        return arrows;
     }
 }
