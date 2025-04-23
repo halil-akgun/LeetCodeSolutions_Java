3528f0c1e43af2373123dd8f72c51820e5c51398
Halil Akgun <halilakgun0@gmail.com>
2025-04-23 22:35:30 +0200

    LC224_BasicCalculator

---
+++ src/TopInterview150/LC224_BasicCalculator.java
@@ -0,0 +1,30 @@
+package TopInterview150;
+
+/*
+Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+Example 1:
+Input: s = "1 + 1"
+Output: 2
+
+Example 2:
+Input: s = " 2-1 + 2 "
+Output: 3
+
+Example 3:
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+ */
+public class LC224_BasicCalculator {
+    public static void main(String[] args) {
+        System.out.println(calculate("1 + 1")); // 2
+        System.out.println(calculate(" 2-1 + 2 ")); // 3
+        System.out.println(calculate("(1+(4+5+2)-3)+(6+8)")); // 23
+    }
+
+    public static int calculate(String s) {
+
+    }
+}
