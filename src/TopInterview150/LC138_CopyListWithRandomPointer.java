ceee50f0179bd9e95a41e9dc60b9c2f7154851be
Halil Akgun <halilakgun0@gmail.com>
2025-05-07 23:59:14 +0200

    LC138_CopyListWithRandomPointer

--- src/TopInterview150/LC138_CopyListWithRandomPointer.java
+++ src/TopInterview150/LC138_CopyListWithRandomPointer.java
@@ -42,9 +42,14 @@ public class LC138_CopyListWithRandomPointer {
     public static void main(String[] args) {
         Node head = new Node(7);
         head.next = new Node(13);
+        head.random = null;
         head.next.next = new Node(11);
+        head.next.random = head;
         head.next.next.next = new Node(10);
+        head.next.next.random = head.next.next.next.next;
         head.next.next.next.next = new Node(1);
+        head.next.next.next.next.next = null;
+        head.next.next.next.next.random = head;
         System.out.println(copyRandomList(head)); // [[7,null],[13,0],[11,4],[10,2],[1,0]]
     }
 
