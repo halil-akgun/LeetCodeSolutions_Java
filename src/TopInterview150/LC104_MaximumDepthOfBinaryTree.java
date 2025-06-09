9ea3749fec3e85666b6b48204cd827c67a8807d2
Halil Akgun <halilakgun0@gmail.com>
2025-06-09 23:56:18 +0200

    LC104_MaximumDepthOfBinaryTree

---
+++ src/TopInterview150/LC104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,49 @@
+package TopInterview150;
+
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+image: https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+ */
+public class LC104_MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root)); // 3
+    }
+
+    public static int maxDepth(TreeNode root) {
+
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
