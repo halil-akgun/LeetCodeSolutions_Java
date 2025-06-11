addedf3b175f2f171f1f6db587d64421529c6aaf
Halil Akgun <halilakgun0@gmail.com>
2025-06-11 23:15:05 +0200

    LC104_MaximumDepthOfBinaryTree

--- src/TopInterview150/LC104_MaximumDepthOfBinaryTree.java
+++ src/TopInterview150/LC104_MaximumDepthOfBinaryTree.java
@@ -26,6 +26,10 @@ public class LC104_MaximumDepthOfBinaryTree {
 
     public static int maxDepth(TreeNode root) {
         if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
     }
 
     static class TreeNode {
