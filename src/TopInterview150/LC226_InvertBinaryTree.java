60300ddaaa45981ba6764f692d687e2d3f062586
Halil Akgun <halilakgun0@gmail.com>
2025-06-13 23:50:33 +0200

    LC226_InvertBinaryTree

---
+++ src/TopInterview150/LC226_InvertBinaryTree.java
@@ -0,0 +1,54 @@
+package TopInterview150;
+
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+image: https://assets.leetcode.com/uploads/2021/03/14/invert1-tree.jpg
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+image: https://assets.leetcode.com/uploads/2021/03/14/invert2-tree.jpg
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+ */
+public class LC226_InvertBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        System.out.println(invertTree(root)); // [4,7,2,9,6,3,1]
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
