7d57ad02a63e3d99f254af74e8aa96bed6b4eaf3
Halil Akgun <halilakgun0@gmail.com>
2025-06-21 23:56:34 +0200

    LC101_SymmetricTree

--- src/TopInterview150/LC101_SymmetricTree.java
+++ src/TopInterview150/LC101_SymmetricTree.java
@@ -40,7 +40,10 @@ public class LC101_SymmetricTree {
     }
 
     private static boolean isMirror(TreeNode root1, TreeNode root2) {
-
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val != root2.val) return false;
+        return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
     }
 
     static class TreeNode {
